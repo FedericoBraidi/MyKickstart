5fbebfdc6cfa710a3e2de5b4f19beae7e72aa215
Federico <federico@braidi.com>
2025-08-24 22:02:23 +0200

    Eliminated odools from init

--- init.lua
+++ init.lua
@@ -736,25 +736,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'whenrow/odoo-ls.nvim',
-    dependencies = { 'neovim/nvim-lspconfig' },
-    config = function()
-      local odools = require 'odools'
-      local h = os.getenv 'HOME'
-      odools.setup {
-        -- mandatory
-        odoo_path = h .. '/Desktop/Roba/Work/odoo',
-        python_path = '/usr/bin/python3',
-
-        -- optional
-        server_path = h .. '/.local/share/nvim/odoo/odoo_ls_server',
-        addons = { h .. '/Desktop/Roba/Work/enterprise' },
-        root_dir = h .. '/Desktop/Roba/Work/odoo',
-      }
-    end,
-  },
-
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
